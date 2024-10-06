ef57eae07ff54d74d9e6359170ad1b63cd3f56a5
mtsonkova <mtsonkova950@gmail.com>
2024-10-07 00:25:32 +0300

    In progress

--- FetchToDoData/index.ts
+++ FetchToDoData/index.ts
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 axios.get(url).then(response => {
     const todo = response.data;
     const id = todo.id;
